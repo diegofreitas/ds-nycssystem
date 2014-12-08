878a541c0811e59e4c555cfded953761b1847301
Diego Freitas <diego.lins.freitas@gmail.com>
2014-12-08 17:41:29 -0400

    improve formula for ols

--- ols.py
+++ ols.py
@@ -6,16 +6,15 @@ import statsmodels.formula.api as smf
 from functions import *
 
 def predictions(weather_turnstile):
-    weather_turnstile['weekday'] = weather_turnstile.DATEn.apply(get_day_week)
-    #weather_turnstile['hour_ampm'] = weather_turnstile.Hour.apply(convert_hour)
-    #weather_turnstile['hour_type'] = weather_turnstile.Hour.apply(ampm_hour)
-
-    features = weather_turnstile[['UNIT','rain', 'Hour', 'meantempi', 'weekday', 'fog', 'thunder']]
+    features = weather_turnstile[['UNIT','rain', 'Hour', 'meantempi', 'weekday']]
     model = smf.ols('ENTRIESn_hourly ~ UNIT * Hour + rain + meantempi + weekday', data=weather_turnstile).fit()
     print model.summary()
     return model.predict(features)
 
-data = pandas.read_csv('turnstile_data_master_with_weather.csv')
+weather_turnstile = pandas.read_csv('turnstile_data_master_with_weather.csv')
+#weather_turnstile['weekday'] = weather_turnstile.DATEn.apply(get_day_week)
+#weather_turnstile['hour_ampm'] = weather_turnstile.Hour.apply(convert_hour)
+#weather_turnstile['hour_type'] = weather_turnstile.Hour.apply(ampm_hour)
 
-predictions = predictions(data)
-print compute_r_squared(data.ENTRIESn_hourly, predictions)
+predictions = predictions(weather_turnstile)
+print compute_r_squared(weather_turnstile.ENTRIESn_hourly, predictions)
